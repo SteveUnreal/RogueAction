b3a9a2c65adfa35801274388f15735d14327698e
Steven Rogers <steven.rogers308@gmail.com>
2022-01-14 15:12:10 -0500

    Adds base files.

---
+++ Source/RogueAction/RogueAction.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RogueAction.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RogueAction, "RogueAction" );
---
+++ Source/RogueAction/RogueActionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RogueActionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROGUEACTION_API ARogueActionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
